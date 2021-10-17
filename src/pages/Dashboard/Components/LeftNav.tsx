986e09497bfb66b8014ae2b94bc8f9403bb0adb5
Toàn Trần <toantran3110@gmail.com>
2021-10-17 18:30:11 +0700

    update code handle chart on dashboard

---
+++ src/pages/Dashboard/Components/LeftNav.tsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import logo from "../../../images/logo.png";
+import sign from "../../../images/logo2.png";
+import icon1 from "../../../images/icon1.png";
+import icon2 from "../../../images/icon2.png";
+import icon3 from "../../../images/icon3.png";
+import icon4 from "../../../images/icon4.png";
+import icon5 from "../../../images/icon5.png";
+import icon6 from "../../../images/icon6.png";
+import icon7 from "../../../images/icon7.png";
+import icon14 from "../../../images/icon14.png";
+
+export default function LeftNav() {
+    return (
+        <div className="dashboard__left">
+                <div className="comp1">
+                    <figure className="logo">
+                        <img src={logo} alt="logo" />
+                    </figure>
+                    <figure className="sign">
+                        <img src={sign} alt="logo" />
+                    </figure>
+                </div>
+                <div className="comp2">
+                    <figure className="icon">
+                        <img src={icon1} alt="icon1" />
+                    </figure>
+                    <p className="text">Login</p>
+                </div>
+                <div className="comp2">
+                    <figure className="icon">
+                        <img src={icon2} alt="icon2" />
+                    </figure>
+                    <p className="text">Sign Up</p>
+                </div>
+                <div className="comp2">
+                    <figure className="icon">
+                        <img src={icon3} alt="icon3" />
+                    </figure>
+                    <p className="text">404 Page</p>
+                </div>
+                <p className="desc">Apps</p>
+                <div className="comp2">
+                    <figure className="icon">
+                        <img src={icon4} alt="icon4" />
+                    </figure>
+                    <p className="text">Project Manage... <figure className="ic"><img src={icon5} alt="ic" /></figure></p>
+                </div>
+                <div className="comp2">
+                    <figure className="icon">
+                        <img src={icon6} alt="icon4" />
+                    </figure>
+                    <p className="text">Knowledge <figure className="ic"><img src={icon5} alt="ic" /></figure></p>
+                </div>
+                <div className="comp2">
+                    <figure className="icon">
+                        <img src={icon7} alt="icon4" />
+                    </figure>
+                    <p className="text">Sales <figure className="ic"><img src={icon5} alt="ic" /></figure></p>
+                </div>
+                <div className="comp2">
+                    <figure className="icon">
+                        <img src={icon7} alt="icon4" />
+                    </figure>
+                    <p className="text">Appointment <figure className="ic"><img src={icon5} alt="ic" /></figure></p>
+                </div>
+                <div className="comp2">
+                    <figure className="icon">
+                        <img src={icon7} alt="icon4" />
+                    </figure>
+                    <p className="text">CRM <figure className="ic"><img src={icon5} alt="ic" /></figure></p>
+                </div>
+                <div className="comp2">
+                    <figure className="icon">
+                        <img src={icon7} alt="icon4" />
+                    </figure>
+                    <p className="text">Chat <figure className="ic"><img src={icon5} alt="ic" /></figure></p>
+                </div>
+                <div className="comp2">
+                    <figure className="icon">
+                        <img src={icon7} alt="icon4" />
+                    </figure>
+                    <p className="text">Documentation <figure className="ic"><img src={icon5} alt="ic" /></figure></p>
+                </div>
+                <div className="comp3">
+                    <figure className="icon">
+                        <img src={icon14} alt="icon4" />
+                    </figure>
+                    <p className="text">John Doe <span>CEO, Founder</span></p>
+                </div>
+            </div>
+    )
+}
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,99 +1,123 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import LeftNav from './Components/LeftNav';
 import './Dashboard.scss';
-import logo from "../../images/logo.png";
-import sign from "../../images/logo2.png";
-import icon1 from "../../images/icon1.png";
-import icon2 from "../../images/icon2.png";
-import icon3 from "../../images/icon3.png";
-import icon4 from "../../images/icon4.png";
-import icon5 from "../../images/icon5.png";
-import icon6 from "../../images/icon6.png";
-import icon7 from "../../images/icon7.png";
-import icon14 from "../../images/icon14.png";
 // import icon15 from "../../images/icon15.png";
+import ApexCharts from 'apexcharts';
 
 export default function Dashboard() {
+    const [tab, setTab] = useState('1')
+
+    useEffect(() => {
+    
+        var options = {
+            series: [{
+            name: 'Number of sent mails',
+            data: [4, 3, 2, 4, 5, 8, 8, 9, 15, 33, 42, 30, 20, 10, 8, 10, 8, 10, 7, 8, 15, 45, 55, 8]
+          }, {
+            name: 'Number of interactions',
+            data: [4, 3, 3, 6, 5, 7, 7, 12, 18, 30, 46, 35, 18, 9, 4, 8, 4, 9, 8, 10, 18, 30, 40, 8]
+          }],
+            chart: {
+            height: 350,
+            type: 'area'
+          },
+          dataLabels: {
+            enabled: false
+          },
+          stroke: {
+            curve: 'smooth'
+          },
+          xaxis: {
+            type: 'datetime',
+            categories: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+          },
+          tooltip: {
+            x: {
+              format: 'dd/MM/yy HH:mm'
+            },
+          },
+          };
+  
+          var chart = new ApexCharts(document.querySelector("#chart"), options);
+          chart.render();
+        }, []);
+
     return (
         <div className="dashboard">
-            <div className="dashboard__left">
-                <div className="comp1">
-                    <figure className="logo">
-                        <img src={logo} alt="logo" />
-                    </figure>
-                    <figure className="sign">
-                        <img src={sign} alt="logo" />
-                    </figure>
-                </div>
-                <div className="comp2">
-                    <figure className="icon">
-                        <img src={icon1} alt="icon1" />
-                    </figure>
-                    <p className="text">Login</p>
-                </div>
-                <div className="comp2">
-                    <figure className="icon">
-                        <img src={icon2} alt="icon2" />
-                    </figure>
-                    <p className="text">Sign Up</p>
-                </div>
-                <div className="comp2">
-                    <figure className="icon">
-                        <img src={icon3} alt="icon3" />
-                    </figure>
-                    <p className="text">404 Page</p>
-                </div>
-                <p className="desc">Apps</p>
-                <div className="comp2">
-                    <figure className="icon">
-                        <img src={icon4} alt="icon4" />
-                    </figure>
-                    <p className="text">Project Manage... <figure className="ic"><img src={icon5} alt="ic" /></figure></p>
-                </div>
-                <div className="comp2">
-                    <figure className="icon">
-                        <img src={icon6} alt="icon4" />
-                    </figure>
-                    <p className="text">Knowledge <figure className="ic"><img src={icon5} alt="ic" /></figure></p>
-                </div>
-                <div className="comp2">
-                    <figure className="icon">
-                        <img src={icon7} alt="icon4" />
-                    </figure>
-                    <p className="text">Sales <figure className="ic"><img src={icon5} alt="ic" /></figure></p>
-                </div>
-                <div className="comp2">
-                    <figure className="icon">
-                        <img src={icon7} alt="icon4" />
-                    </figure>
-                    <p className="text">Appointment <figure className="ic"><img src={icon5} alt="ic" /></figure></p>
-                </div>
-                <div className="comp2">
-                    <figure className="icon">
-                        <img src={icon7} alt="icon4" />
-                    </figure>
-                    <p className="text">CRM <figure className="ic"><img src={icon5} alt="ic" /></figure></p>
-                </div>
-                <div className="comp2">
-                    <figure className="icon">
-                        <img src={icon7} alt="icon4" />
-                    </figure>
-                    <p className="text">Chat <figure className="ic"><img src={icon5} alt="ic" /></figure></p>
-                </div>
-                <div className="comp2">
-                    <figure className="icon">
-                        <img src={icon7} alt="icon4" />
-                    </figure>
-                    <p className="text">Documentation <figure className="ic"><img src={icon5} alt="ic" /></figure></p>
-                </div>
-                <div className="comp3">
-                    <figure className="icon">
-                        <img src={icon14} alt="icon4" />
-                    </figure>
-                    <p className="text">John Doe <span>CEO, Founder</span></p>
-                </div>
-            </div>
+            <LeftNav />
+            
             <div className="dashboard__right">
-                
+                <div className="top">
+                    <p className="text">Event-15</p>
+                    <select name="event" id="event">
+                        <option value="event-15">Event-15</option>
+                        <option value="event-15">Event-14</option>
+                        <option value="event-15">Event-13</option>
+                    </select>
+                </div>
+                <div className="bottom">
+                    <div className="bottom__left">
+                        <div className="chart">
+                            <div className="wrap">
+                                <p className="title">Event Overview</p>
+                                <div className="filter">
+                                    <p className="text2 active">Day</p>
+                                    <p className="text2">Week</p>
+                                </div>
+                            </div>
+                            <div className="wrap-chart">
+                                <div id="chart"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bottom__right">
+                        <p className="title">Top Exhibitors</p>
+                        <div className="list">
+                            <div className="item">
+                                <div className="wrap">
+                                    <span>RA</span>
+                                    <p>Viatick</p>
+                                </div>
+                                <p className="text3">288 visitors</p>
+                            </div>
+                            <div className="item">
+                                <div className="wrap">
+                                    <span>RA</span>
+                                    <p>Viatick</p>
+                                </div>
+                                <p className="text3">288 visitors</p>
+                            </div>
+                            <div className="item">
+                                <div className="wrap">
+                                    <span>RA</span>
+                                    <p>Viatick</p>
+                                </div>
+                                <p className="text3">288 visitors</p>
+                            </div>
+                            <div className="item">
+                                <div className="wrap">
+                                    <span>RA</span>
+                                    <p>Viatick</p>
+                                </div>
+                                <p className="text3">288 visitors</p>
+                            </div>
+                            <div className="item">
+                                <div className="wrap">
+                                    <span>RA</span>
+                                    <p>Viatick</p>
+                                </div>
+                                <p className="text3">288 visitors</p>
+                            </div>
+                            <div className="item">
+                                <div className="wrap">
+                                    <span>RA</span>
+                                    <p>Viatick</p>
+                                </div>
+                                <p className="text3">288 visitors</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )

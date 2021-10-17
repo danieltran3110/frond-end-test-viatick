d3dea07779b24cabf3a64ad50adcf64f0363f0e7
Toàn Trần <toantran3110@gmail.com>
2021-10-17 17:45:50 +0700

    update code handle signup and dashboard page 10%

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 import SignIn from './pages/SignIn/SignIn';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
+    const [login, setLogin] = useState(false);
   return (
     <Router>
         <Switch>
-            <Route path="/">
+            <Route path="/signIn">
                 <SignIn />
+            </Route>
+            <Route path="/dashboard">
+                <Dashboard />
+            </Route>
+            <Route exact path="/">
+                {login ? <Redirect to={'/dashboard'} /> : <Redirect to={'/signIn'} />}
             </Route>
         </Switch>
     </Router>
--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,19 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import './SignIn.scss';
 
 export default function SignIn() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useHistory();
+
+    const handleSubmitLogin = (e: any) => {
+        e.preventDefault();
+        history.push('/dashboard');
+    }
+
     return (
         <div className="signin">
             <div className="signin__inner">
                 <h1 className="signin__title">Welcome,</h1>
                 <div className="signin__desc">Sign in to continue!</div>
-                <form className="signin__form">
-                <input className="input" type="email" name="email" placeholder="Email" />
-                <input className="input" type="password" name="password" placeholder="Password" />
-                <a href="#">Forget password?</a>
-                <input type="submit" value="Continue" />
+                <form className="signin__form" onSubmit={(e) => handleSubmitLogin(e)}>
+                    <input className="input" value={email} onChange={(e) => setEmail(e.currentTarget.value)} type="email" name="email" placeholder="Email" required/>
+                    <input className="input" value={password} onChange={(e) => setPassword(e.currentTarget.value)} type="password" name="password" placeholder="Password" required/>
+                    <a className="link" href="#">Forget password?</a>
+                    <input className="btn-submit" type="submit" value="Continue" />
                 </form>
             </div>
+            <div className="layer1"></div>
+            <div className="layer2"></div>
         </div>
     )
 }
